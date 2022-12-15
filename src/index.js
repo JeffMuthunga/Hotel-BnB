6efc94b9fa982d72af6853ce4196039aaae0aef0
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2022-12-15 03:29:30 +0300

    Fetch Data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const template = document.querySelector(".place-here") 
+console.log(template)
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+    fetchData().then((data) => { 
+        data.record.forEach(element => {
+            
+        });
+    }) 
+
+
+
+})
+
 
 function fetchData(){
 return fetch("https://api.jsonbin.io/v3/b/6399fda8dfc68e59d5681ece")
@@ -5,4 +20,4 @@ return fetch("https://api.jsonbin.io/v3/b/6399fda8dfc68e59d5681ece")
 .then(data => data)
 }
 
-fetchData().then((data) => console.log(data.record[0]))
+
